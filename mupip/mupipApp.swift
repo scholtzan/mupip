0bddafeac751f92313677d6ff0ae2a4a14a2f4be
Anna Scholtz <anna@scholtzan.net>
2022-12-30 16:01:55 -0800

    Initial Commit

---
+++ mupip/mupipApp.swift
@@ -0,0 +1,17 @@
+//
+//  mupipApp.swift
+//  mupip
+//
+//  Created by Anna Scholtz on 2022-12-30.
+//
+
+import SwiftUI
+
+@main
+struct mupipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
